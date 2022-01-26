8c8bf7bd9d320ad25c395d731e5c248fa21cb2d4
doa10282001 <92369789+doa10282001@users.noreply.github.com>
2022-01-26 14:02:44 +0800

    Update 12-2.py

--- 12-2.py
+++ 12-2.py
@@ -8,9 +8,10 @@ plt.xlabel('Probability')
 plt.ylabel('Likelihood')
 
 x_plt = 2/3
+y_plt = -x_plt**3 + x_plt**2
 new_x = [2/3-0.1, 2/3, 2/3+0.1]
 new_y = [-x_plt**3 + x_plt**2 for i in new_x ] 
-
-plt.plot(new_x, new_y, '-o', color='r')
+plt.plot(x_plt, y_plt, '-o', color='r')
+plt.plot(new_x, new_y, color='r')
 plt.grid()
 plt.show()
